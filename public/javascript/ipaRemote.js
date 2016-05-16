8ca968fc804433ad46a7bba45578a1f54799dbde
Tarek Sk <atrulz10@gmail.com>
2016-05-16 20:57:10 +0000

    dfdfd

--- public/javascript/ipaRemote.js
+++ public/javascript/ipaRemote.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Service names
-var IPA_DIALOG_NAME = "dialog-service-141516"; // REPLACE with your own name
+var IPA_DIALOG_NAME = "dialog-service-14151617"; // REPLACE with your own name
 var IPA_CLASSIFIER_NAME = "nlc-service-1413";  // NO NEED TO RENAME THIS ONE unless you want to
 
 var find_dialog_name = "";
